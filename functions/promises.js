224bbf33a530e4667b014b3a39c5fcb0555c5c42
kshly <kishal.1994@gmail.com>
2020-05-13 15:16:35 +0530

    Function updates

--- functions/promises.js
+++ functions/promises.js
@@ -1,17 +1,29 @@
-//using callback
-const getData = ((callback)=>{
+//using callback 
+const getDataCallBack = ((num, callback)=>{
     setTimeout(()=>{
-        callback(undefined, 'This is the data')
+        if(typeof num === 'number'){
+            callback(undefined , num*2)
+        }
+        else{
+            callback('The number must be provided', undefined)
+        }
     }, 2000)
 })
 
-
-getData((err , data) =>{
+//implementation of callback hell
+getDataCallBack(2,(err , data) =>{
     if(err){
         return `Error: ${err}`
     }
     else{
-        console.log(data)
+        getDataCallBack(data , (err, data) =>{
+            if(err){
+                console.log(err)
+            }
+            else{
+                console.log(data)
+            }
+        })
     }
 })
 
@@ -28,5 +40,33 @@ const myPromise = getDataPromise(112)
 myPromise.then((data) => {
     console.log(data)
     }, (err)=> {
+    console.log(err)
+})
+
+//implementation of promise chain 
+const getPromise = (num) => new Promise((resolve, reject) =>{
+    setTimeout(() =>{
+        typeof num === 'number' ? resolve(num*2) : reject('Number must be provided')
+    }, 2000)
+})
+
+//without using promise chaining
+getPromise(2).then((data)=>{
+    getPromise(data).then((data) =>{
+        console.log(`Tha data is ${data}`)
+    },(err) =>{
+        console.log(err)
+    })
+}, (err)=>{
+    console.log(err)
+})
+
+//using promise chaining 
+//if anyone of the promise in the promise chain fails it directly goes to catch
+getPromise(10).then((data) =>{
+    return getPromise(data)
+}).then((data) => {
+    console.log(data)
+}).catch((err) => {
     console.log(err)
 })
